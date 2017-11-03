2bf8e28a1ab768643d1280803c47f1f119a08eaf
user1211 <badabing@someemail.com>
2017-11-03 13:38:18 +0100

    combining parallel and filtering

--- src/main/java/com/c2_systems/benchmark/MyBenchmark.java
+++ src/main/java/com/c2_systems/benchmark/MyBenchmark.java
@@ -5,6 +5,17 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.math3.filter.DefaultMeasurementModel;
+import org.apache.commons.math3.filter.DefaultProcessModel;
+import org.apache.commons.math3.filter.KalmanFilter;
+import org.apache.commons.math3.filter.MeasurementModel;
+import org.apache.commons.math3.filter.ProcessModel;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.random.JDKRandomGenerator;
+import org.apache.commons.math3.random.RandomGenerator;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Measurement;
@@ -44,14 +55,39 @@ public class MyBenchmark implements Function<Integer, Integer> {
 
     Random randomNum = new Random();
 
+    KalmanFilter kfilter;
+    RealVector u;
+    RealVector x;
+    RealMatrix A;
+    RealMatrix B;
+    RealMatrix H;
+    double dt;
+    double measurementNoise;
+    double accelNoise;
+
     Flowable<Integer> parallel;
     Flowable<Integer> notsoparallel;
     Flowable<Integer> zippedparallel;
 
     @Override
     public Integer apply(Integer t) throws Exception {
-        Blackhole.consumeCPU(compute);
-        return t;
+
+    	RandomGenerator rand = new JDKRandomGenerator();
+        RealVector tmpPNoise = new ArrayRealVector(new double[] { Math.pow(dt, 2d) / 2d, dt });
+        RealVector mNoise = new ArrayRealVector(1);
+
+        kfilter.predict(u);
+        RealVector pNoise = tmpPNoise.mapMultiply(accelNoise * rand.nextGaussian());
+        x = A.operate(x).add(B.operate(u)).add(pNoise);
+        mNoise.setEntry(0, measurementNoise * rand.nextGaussian());
+        RealVector z = H.operate(x).add(mNoise);
+
+        kfilter.correct(z);
+
+        Double position = kfilter.getStateEstimation()[0];
+        Double velocity = kfilter.getStateEstimation()[1];
+
+        return velocity.intValue();
     }
 
     @Setup
@@ -71,6 +107,34 @@ public class MyBenchmark implements Function<Integer, Integer> {
 
         Flowable<Integer> source = Flowable.fromArray(ints);
         Flowable<Integer> anothersource = Flowable.fromArray(moreints);
+
+
+        dt = 0.1d;
+        measurementNoise = 10d;
+        accelNoise = 0.2d;
+
+        A = new Array2DRowRealMatrix(new double[][] { { 1, dt }, { 0, 1 } });
+        B = new Array2DRowRealMatrix(new double[][] { { Math.pow(dt, 2d) / 2d }, { dt } });
+        H = new Array2DRowRealMatrix(new double[][] { { 1d, 0d } });
+        x = new ArrayRealVector(new double[] { 0, 0 });
+
+        RealMatrix tmp = new Array2DRowRealMatrix(new double[][] {
+            { Math.pow(dt, 4d) / 4d, Math.pow(dt, 3d) / 2d },
+            { Math.pow(dt, 3d) / 2d, Math.pow(dt, 2d) } });
+
+        RealMatrix Q = tmp.scalarMultiply(Math.pow(accelNoise, 2));
+
+        RealMatrix P0 = new Array2DRowRealMatrix(new double[][] { { 1, 1 }, { 1, 1 } });
+
+        RealMatrix R = new Array2DRowRealMatrix(new double[] { Math.pow(measurementNoise, 2) });
+
+        u = new ArrayRealVector(new double[] { 0.1d });
+
+        ProcessModel pm = new DefaultProcessModel(A, B, Q, x, P0);
+        MeasurementModel mm = new DefaultMeasurementModel(H, R);
+        kfilter = new KalmanFilter(pm, mm);
+
+
 
         parallel = source.parallel(cpu).runOn(Schedulers.computation()).map(this).sequential();
 
